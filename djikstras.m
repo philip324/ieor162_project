01cb69cfae1af96521634e80b72b7f510faebb51
wesgraham <38120678+wesgraham@users.noreply.github.com>
2018-11-18 15:14:25 -0800

    Updated what is being returned

--- djikstras.m
+++ djikstras.m
@@ -1,33 +1,36 @@
-function visual = vertexedge(map)
+function costs = vertexedgecosts(map)
 %takes a set of nodes and maps out the paths between them
 
 %start by mapping out where the vertices are
-
-
-%then calculate the distance between each vertex
 
 
 %then figure out what transportation methods exist between the start_node
 %and end node
 
 
-%then calculate the cost of traversing that edge
-
-%do so for every edge & vertex in the network
+%then calculate the distance between each vertex
 
 
-%return a visual showing the vertices, edges, and edge lengths
+%then calculate the cost of travelling that distance
+
+
+%repeat this for every edge in the network
+
+
+%return a cell containing start, end, cost for each pair
 
 end
 
 function route = djikstra(start_node, end_node, map)
-%takes a set of paths, start node, and end node, and returns the shortest
-%path between them
+%takes a cell containing  nodes and costs and returns the shortest
+%path between a start and end node in the set
 
-%load the visual/data from the above map
+%load the cell from the above function
 
 
 %run djikstra's algorithm from the start node to the end node
 
+%return the resulting shortest path as an array where each entry is the
+%node traversed
 
 end
